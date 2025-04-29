760e2e6c4abcbc5b8d7053935dd6a706a7b4b7a5
Jakob Lagle <87400188+jlagle@users.noreply.github.com>
2025-04-28 22:36:46 -0400

    drawing assignemnt

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+    //creates a canvas 600 pixels wide
+    //and 400 pixels high
+    createCanvas(600, 400);
+}
+function draw() {
+    //sky blue background
+    background(135, 206, 235);
+    //sun in top right
+    fill("yellow");//yellow  
+  
+    stroke("orange"); //orange outline 
+  
+    strokeWeight(20); //large outline    
+  
+    circle(550, 50, 100);
+   
+    //grass on bottom half
+  
+    stroke(0);//black outline
+  
+    strokeWeight(1);//outline thickness
+  
+    fill("green");
+  
+    rect(0, 200, 600, 200);
+   
+    //emojis
+    textSize(75)
+    text("🌸", 100, 250) //flower
+    text("🐞", 300, 250) //ladybug
+}
